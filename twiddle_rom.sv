032abaa2b38f1d84129eb7cc8377fbb9d943baa4
Paddy-Zheng <Paddy.zheng@stonybrook.edu>
2026-06-16 17:32:26 -0400

    fixed syntax errors, and added twiddle rom tb

--- twiddle_rom.sv
+++ twiddle_rom.sv
@@ -1,9 +1,10 @@
-parameter N = 1024;
-parameter DEPTH = N/2;
-paremeter ABITS = 9; //address bits
-parameter WIDTH = 16; //
 
-module twiddle_rom (
+module twiddle_rom #(
+    parameter N = 1024,
+    parameter DEPTH = N/2,
+    parameter ABITS = 9, //address bits
+    parameter WIDTH = 16 //width of the twiddle factor in bits
+)(
     input logic clk,
     input logic [ABITS-1:0] addr,
     output logic signed [WIDTH-1:0] wr,
@@ -12,7 +13,7 @@ module twiddle_rom (
     logic signed [WIDTH-1:0] rom_real[0:DEPTH-1];
     logic signed [WIDTH-1:0] rom_imag[0:DEPTH-1];
 
-    initial begein
+    initial begin
         $readmemh("twiddle_real.hex", rom_real);
         $readmemh("twiddle_imag.hex", rom_imag);
     end
