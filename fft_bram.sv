676578db8de3727355411d5f91070f222f56777c
Faid Faisal <ffaisal@bnl.gov>
2026-06-16 14:38:51 -0400

    Update fft_bram.sv

--- fft_bram.sv
+++ fft_bram.sv
@@ -1,15 +1,19 @@
-module bram (
-    parameter ABITS = 10, // Address bits
-    parameter WIDTH = 32, // Data width
-    parameter DEPTH = 1024 // Depth of the BRAM
+module fft_bram #(
+    parameter ABITS = 10,
+    parameter WIDTH = 32,
+    parameter DEPTH = 1024
 )(
     input logic clk,
+
     input logic [ABITS-1:0] addrA,
     input logic [ABITS-1:0] addrB,
+
     input logic signed [WIDTH-1:0] data_inA,
     input logic signed [WIDTH-1:0] data_inB,
+
     output logic signed [WIDTH-1:0] data_outA,
     output logic signed [WIDTH-1:0] data_outB,
+
     input logic weA,
     input logic weB
 );
